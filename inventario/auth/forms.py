fc4ba8ee08d8aa10b7bc0f366f41421e67dc9c91
Gabriel Malachias Hangrad <gbl.malachias@gmail.com>
2025-07-27 16:19:02 -0300

    Update
    Realizado o formulario de registro e as validações de senha, também criado o registro.html e

--- inventario/auth/forms.py
+++ inventario/auth/forms.py
@@ -1,10 +1,49 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, EmailField
 from wtforms.validators import DataRequired, Email, Length, ValidationError, EqualTo
+import re
+
+
+def valida_caracteres(form, field):
+    senha = field.data
+    if not re.search(r'[^A-Za-z0-9]', senha):
+        raise ValidationError(
+            'A senha deve conter pelo menos um caraterer especial, como [!@#$%]')
 
 
 class FormLogin(FlaskForm):
-    usuario = StringField('Usuario', validators=[DataRequired(message='Campo usuarío é obrigatório')])
-    senha = PasswordField('Senha', validators=[DataRequired(message='Campo senha é obrigatório')])
+    usuario = StringField('Usuario', validators=[
+        DataRequired(message='Campo usuarío é obrigatório')])
+    senha = PasswordField('Senha', validators=[
+        DataRequired(message='Campo senha é obrigatório')])
     enviar = SubmitField('Fazer Login')
 
+
+class FormRegistro(FlaskForm):
+    usuario = StringField('Usuario', validators=[
+        DataRequired(message='Campo Obrigatório'),
+        Length(min=8, message='Campo deve ter 8 digitos')])
+    nome_completo = StringField('Nome Completo', validators=[
+        DataRequired(message='Campo Obrigatório')
+    ])
+    email = EmailField('E-mail', validators=[
+        DataRequired(message='Por favor, ensira seu e-mail'),
+        Email(message='Digite um e-mail valido')])
+
+    confirmar_email = EmailField('Confirmar  o e-mail', validators=[
+        DataRequired(message='Ensira novamente seu e-mail'),
+        EqualTo('email', message='Os e-mails devem ser iguais'),
+        Email(message='Digite um e-mail valido!')])
+
+    senha = PasswordField('Senha', validators=[
+        DataRequired(message='Campo Obrigatório'),
+        Length(min=12, message='Campo deve ter no minimo 12 caracteres'),
+        valida_caracteres
+    ])
+    confirmar_senha = PasswordField('Confirmar a senha', validators=[
+        DataRequired(message='Campo Obrigatório'),
+        Length(min=12),
+        EqualTo('senha', message='As senhas devem ser iguais'),
+    ])
+    
+    enviar = SubmitField('Cadastrar')
--- inventario/auth/routes.py
+++ inventario/auth/routes.py
@@ -1,14 +1,25 @@
-from flask import render_template, Blueprint
-from inventario.auth.forms import FormLogin
+from flask import render_template, Blueprint, url_for, redirect
+from inventario.auth.forms import FormLogin, FormRegistro
 
-auth = Blueprint('auth', __name__, template_folder='templates')
+auth = Blueprint('auth', __name__, template_folder='templates', static_folder='static')
 
 
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
     form = FormLogin()
     
     if form.validate_on_submit():
         ...
     return render_template('login.html', form=form)
+
+
+@auth.route('/registro')
+def registro():
+    form = FormRegistro()
+    
+    print("URL Gerada:", url_for('auth.static', filename='css/registro.css'))
+    if form.validate_on_submit():
+        ...
+    
+    return render_template('registro.html', form=form)
